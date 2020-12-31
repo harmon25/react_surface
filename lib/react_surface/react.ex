26fcb4d51693c1dbcd77d63baffc2d70a2d182d3
harmon25 <dougwright1@gmail.com>
2020-12-31 13:43:38 -0500

    remove binding prop

--- lib/react_surface/react.ex
+++ lib/react_surface/react.ex
@@ -7,7 +7,6 @@ defmodule ReactSurface.React do
   prop container_class, :css_class, default: []
   prop opts, :keyword, default: []
   prop container, :atom, values: [:div, :span, :p], default: :div
-  prop binding_prefix, :string, default: "phx"
   prop hook_name, :string, default: "__ReactSurface"
 
   def render(%{container: :div} = assigns) do
@@ -28,14 +27,11 @@ defmodule ReactSurface.React do
     """
   end
 
-  defp build_attrs(%{component: component, props: props_map, binding_prefix: prefix, hook_name: hkname} = props, other_attrs \\ []) do
-    encoded = [component, props_map]
-    |> Jason.encode!()
+  defp build_attrs(%{component: component, props: props_map, hook_name: hkname} = props, other_attrs \\ []) do
+    encoded = Jason.encode!([component, props_map])
 
-    other_attrs
-    |> Keyword.merge(["rs-comp": encoded, "#{prefix}-hook": hkname, id: build_id(props)])
+    Keyword.merge(other_attrs, ["rs-comp": encoded, "phx-hook": hkname, id: build_id(props)])
   end
-
 
   defp build_id(%{id: nil, component: comp}), do: comp
   defp build_id(%{id: id, component: comp}), do: "#{id}_#{comp}"
